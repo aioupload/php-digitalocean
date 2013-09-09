4f81ea75606467af7c74124b3baf0984d9698dfe
Jonathan <lovattj@me.com>
2013-09-09 14:23:22 +0100

    Updated error handling.

--- functions.php
+++ functions.php
@@ -14,14 +14,14 @@ class DigitalOcean {
 	}
 
 	// Retrieves a list of droplets on your account.
-	// Returns a multidimensional array containing droplet info on success, or false on failure.
+	// Returns a multidimensional array containing droplet info on success, or throws an exception on error.
 	public function droplets() {
 		$response = $this->make_get_request("droplets/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['droplets'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -29,7 +29,7 @@ class DigitalOcean {
 	}
 	
 	// Retrieves a count of droplets on your account.
-	// Returns the value, or false on failure.
+	// Returns the value.
 	public function droplets_count() {
 		$response = $this->droplets();
 		if ($response) {
@@ -40,14 +40,14 @@ class DigitalOcean {
 	}
 	
 	// Retrieves information for specified $dropletid.
-	// Returns an array on success.
+	// Returns an array on success or throws an exception on error or null return.
 	public function droplet_info($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['droplet'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -55,29 +55,29 @@ class DigitalOcean {
 	}
 	
 	// Creates a new Droplet.
-	// Returns an array of new droplet parameters on success, or false on failure.
+	// Returns an array of new droplet parameters on success or throws an exception on error.
 	public function droplet_new($name, $size_id, $image_id, $region_id) {
 		$response = $this->make_get_request("droplets/new?name=".$name."&size_id=".$size_id."&image_id=".$image_id."&region_id=".$region_id."&");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['droplet'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
-			return false;
+			throw new Exception('Non-200 HTTP status code. Check your API key or method');
 		}
 	}
 		
 	// Reboots a droplet.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success or throws an exception on error.
 	public function droplet_reboot($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/reboot/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -85,14 +85,14 @@ class DigitalOcean {
 	}
 	
 	// Power-cycles a droplet.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success or throws an exception on error.
 	public function droplet_power_cycle($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/power_cycle/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -100,14 +100,14 @@ class DigitalOcean {
 	}
 
 	// Shuts-down a droplet (but you still continue to be charged).
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_shutdown($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/shutdown/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -115,14 +115,14 @@ class DigitalOcean {
 	}
 	
 	// Hard powers-off a droplet (but you still continue to be charged).
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_power_off($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/power_off/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -130,14 +130,14 @@ class DigitalOcean {
 	}
 	
 	// Powers-on a droplet that was previously shut down or powered-off.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_power_on($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/power_on/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -145,14 +145,14 @@ class DigitalOcean {
 	}
 	
 	// Resets the password on a droplet. The new password is e-mailed to the account e-mail address
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_password_reset($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/password_reset/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -160,14 +160,14 @@ class DigitalOcean {
 	}	
 
 	// Destroys a droplet (you stop being charged for it). This is irreversable.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_destroy($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/destroy/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -175,14 +175,14 @@ class DigitalOcean {
 	}	
 
 	// Takes a snapshot of the droplet. This may cause a reboot.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_take_snapshot($dropletid, $snapshotname) {
 		$response = $this->make_get_request("droplets/".$dropletid."/snapshot/?name=".$snapshotname."&");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -190,14 +190,14 @@ class DigitalOcean {
 	}
 	
 	// Resizes the droplet. First call sizes() to get the ID of the size you want.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_resize($dropletid, $sizeid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/resize/?size_id=".$sizeid."&");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -205,14 +205,14 @@ class DigitalOcean {
 	}
 	
 	// Erases and rebuilds the droplet with a default image. First call images() to get the ID of the image you want.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_rebuild($dropletid, $imageid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/rebuild/?image_id=".$imageid."&");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -220,14 +220,14 @@ class DigitalOcean {
 	}	
 	
 	// Renames the droplet.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_rename($dropletid, $newname) {
 		$response = $this->make_get_request("droplets/".$dropletid."/rename/?name=".$newname."&");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -235,14 +235,14 @@ class DigitalOcean {
 	}	
 
 	// Enables automated backups on a droplet.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_enable_backups($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/enable_backups/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -250,14 +250,14 @@ class DigitalOcean {
 	}
 	
 	// Disables automated backups on a droplet.
-	// Returns the event_id on success, or false on failure.
+	// Returns the event_id on success, or throws an exception on error.
 	public function droplet_disable_backups($dropletid) {
 		$response = $this->make_get_request("droplets/".$dropletid."/disable_backups/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['event_id'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -265,14 +265,14 @@ class DigitalOcean {
 	}
 	
 	// Retrieves a list of available droplet sizes and ID's, used for creating or resizing droplets.
-	// Returns a multidimensional array of droplet sizes and IDs on success, or false on failure.
+	// Returns a multidimensional array of droplet sizes and IDs on success, or throws an exception on error.
 	public function sizes() {
 		$response = $this->make_get_request("sizes/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['sizes'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -280,14 +280,14 @@ class DigitalOcean {
 	}
 	
 	// Retrieves a list of available regions and ID's, used for creating droplets.
-	// Returns a multidimensional array of regions, or false on failure.
+	// Returns a multidimensional array of regions, or throws an exception on error.
 	public function regions() {
 		$response = $this->make_get_request("regions/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['regions'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -295,14 +295,14 @@ class DigitalOcean {
 	}
 	
 	// Retrieves a list of available machine-images - public images and snapshots/backups you've made.
-	// Returns a multidimensional array of machine-images, or false on failure.
+	// Returns a multidimensional array of machine-images, or throws an exception on error.
 	public function images() {
 		$response = $this->make_get_request("images/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['images'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -321,14 +321,14 @@ class DigitalOcean {
 	}	
 	
 	// Retrieves information on the specified machine-image.
-	// Returns an array on success.
+	// Returns an array on success, or throws an exception on error.
 	public function image_info($imageid) {
 		$response = $this->make_get_request("images/".$imageid."/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return $response[1]['image'];
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -336,14 +336,14 @@ class DigitalOcean {
 	}
 	
 	// Destroys the specified machine-image.
-	// Returns true on success, or false on failure.
+	// Returns true on success, or throws an exception on error.
 	public function image_destroy($imageid) {
 		$response = $this->make_get_request("images/".$imageid."/destroy/?");
 		if ($this->was_response_ok($response[0])) {
 			if ($response[1]['status'] == "OK") {
 				return true;
 			} else {
-				return false;
+				throw new Exception($response[1]['error_message']);
 			}
 		} else {
 			throw new Exception('Non-200 HTTP status code. Check your API key or method');
@@ -351,7 +351,7 @@ class DigitalOcean {
 	}
 		
 	// Retrieves information on a specified event, e.g. so you can monitor long-running events.
-	// Returns an array on success.
+	// Returns an array on success, or throws an exception on error.
 	public function event_info($eventid) {
 		$response = $this->make_get_request("events/".$eventid."/?");
 		if ($this->was_response_ok($response[0])) {
